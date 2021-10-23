6ef8597e42cdb3255a939b87d002450d8e9d2a63
olumide azeez afolabi <midas44000@yahoo.com>
2021-10-23 12:41:02 -0500

    initial commit

---
+++ OperatorExerciseConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OperatorExerciseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 17;
+            double b = 4;
+            double sum;
+            sum = a + b;
+            double minus;
+            minus = a - b;
+            double multiply;
+            multiply = a * b;
+            double divide;
+            divide = a / b;
+
+            double qoutient;
+            qoutient = a / b;
+            double remainder;
+            remainder = a % b;
+
+            Console.WriteLine($" {sum}. {minus}. {multiply}. {divide}. {remainder}.");
+
+
+
+            
+            
+
+        }
+    }
+}
